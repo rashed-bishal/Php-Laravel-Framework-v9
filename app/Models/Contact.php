66d5db6e604744b5d6d292bfb8a1357d4a0ddb96
Rashed Bishal <rashed.bishal@gmail.com>
2023-03-25 14:20:05 +0600

    filling a missing eloquent function in Contact Class

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Company;
 
 class Contact extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+
 }
