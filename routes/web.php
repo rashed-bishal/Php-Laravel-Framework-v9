9781a120fbfaf1d62477d1437e941c757db1353f
rashed-bishal <rashed.bishal@gmail.com>
2023-03-26 15:43:44 +0600

    Adding Controllers

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ContactController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,17 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/contacts', function(){
-    return view('contacts.index');
-})->name('contacts.index');
+Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
 
 
-Route::get('/contacts/create', function(){
-    return view('contacts.create');
-})->name('contacts.create');
+Route::get('/contacts/create', [ContactController::class, 'create'])->name('contacts.create');
 
 
-Route::get('/contacts/{id}', function($id){
-    $contact = \App\Models\Contact::find($id);
-    return view('contacts.show', compact('contact'));
-})->name('contacts.show');
+Route::get('/contacts/{id}', [ContactController::class, 'show'])->name('contacts.show');
