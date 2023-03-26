93da49657f0948f8ffa8a0047d7c0705acfb5232
rashed-bishal <rashed.bishal@gmail.com>
2023-03-26 17:26:52 +0600

    Application Layout and blade template inheretance

---
+++ resources/views/contacts/show.blade.php
@@ -0,0 +1,7 @@
+<h2>Showing a contact</h2>
+
+<p>{{$contact}}</p>
+
+<p><div class="class">
+	<a href="{{route('contacts.index')}}">Back to all contacts</a>
+</div></p>
---
+++ resources/views/includes/navbar.blade.php
@@ -0,0 +1,31 @@
+<nav class="navbar navbar-expand-lg navbar-light">
+      <div class="container">
+        <a class="navbar-brand text-uppercase" href="index.html">            
+            <strong>Contact</strong> App
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-toggler" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+            
+        <!-- /.navbar-header -->
+        <div class="collapse navbar-collapse" id="navbar-toggler">
+          <ul class="navbar-nav">
+            <li class="nav-item"><a href="#" class="nav-link">Companies</a></li>
+            <li class="nav-item active"><a href="#" class="nav-link">Contacts</a></li>
+          </ul>
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item mr-2"><a href="#" class="btn btn-outline-secondary">Login</a></li>
+            <li class="nav-item"><a href="#" class="btn btn-outline-primary">Register</a></li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                John Doe
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <a class="dropdown-item" href="profile.html">Settings</a>
+                <a class="dropdown-item" href="#">Logout</a>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
