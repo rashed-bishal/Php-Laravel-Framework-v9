047c2151d62958e6f1c5c13f251db816733a2e29
rashed-bishal <rashed.bishal@gmail.com>
2023-03-26 19:43:03 +0600

    All Companies Added to dropdown list

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -5,13 +5,15 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Contact;
+use App\Models\Company;
 
 class ContactController extends Controller
 {
     public function index()
     {
+        $companies = Company::orderBy('name','asc')->pluck('name', 'id');
         $contacts = Contact::select('first_name', 'last_name', 'email', 'company_id')->orderBy('first_name', 'asc')->paginate(5);
-        return view('contacts.index', compact('contacts'));
+        return view('contacts.index', compact('contacts', 'companies'));
     }
 
     public function create()
