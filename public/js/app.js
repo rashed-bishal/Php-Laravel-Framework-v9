6c46f0853b29c430918255e2577e90b738846b57
Rashed Bishal <rashed.bishal@gmail.com>
2023-03-31 14:36:14 +0600

    Dynamic Refresh Button Enabled

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,20 @@
 $(document).ready(function(){
+	$('#search').keyup(function(event){
+		let searchValue = $(this).val();
+		if(searchValue.length == 0)
+		{
+			$('#btn-refresh').fadeOut();
+		}
+		else
+		{
+			$('#btn-refresh').fadeIn();
+		}
+
+	});	
+});
+
+
+$(document).ready(function(){
 	$('#companies_id').change(function(){
 		let selectedValue = $(this).val();
 		window.location.href = "/contacts/company/"+selectedValue;
@@ -9,5 +25,9 @@ $(document).ready(function(){
 			url: '/contacts/erase',
 			method: 'POST',
 		});
+	});
+
+	$('#btn-refresh').click(function(){
+		window.location.href = "/contacts";
 	});
 });
--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -37,9 +37,9 @@
                       </div>
                       <div class="col">
                         <div class="input-group mb-3">
-                          <input type="text" class="form-control" name="search" id="search" placeholder="Search..." aria-label="Search..." aria-describedby="button-addon2">
+                          <input type="text" class="form-control" name="search" id="search" value="{{request('search')}}" placeholder="Search..." aria-label="Search..." aria-describedby="button-addon2">
                           <div class="input-group-append">
-                              <button class="btn btn-outline-secondary" type="button">
+                              <button class="btn btn-outline-secondary" id="btn-refresh" type="button">
                                   <i class="fa fa-refresh"></i>
                                 </button>
                             <button class="btn btn-outline-secondary" type="submit" id="button-addon2">
